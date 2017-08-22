fbb5df34098f6a0cc31701701c62aa6a05fbba9c
TheRoyalTnetennba <gpaye8@gmail.com>
2017-08-21 20:03:04 -0700

    rework email form to intelligently set body and subject

--- controllers/email.go
+++ controllers/email.go
@@ -3,22 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
-	"net/http/httputil"
 	"net/smtp"
 )
 
 func SendEmail(w http.ResponseWriter, r *http.Request) {
-	requestDump, err := httputil.DumpRequest(r, true)
-	body := string(requestDump)
+	body := r.FormValue("Greeting")
 	from := FromEmail
 	pass := EmailPass
-	to := "gpaye8@gmail.com"
+	to := ToEmail
 
 	msg := "From: " + from + "\n" +
 		"To: " + to + "\n" +
-		"Subject: Hello there\n\n" +
+		"Subject: " + r.Host + "\n\n" +
 		body
-	err = smtp.SendMail("smtp.gmail.com:587",
+	err := smtp.SendMail("smtp.gmail.com:587",
 		smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
 		from, []string{to}, []byte(msg))
 
@@ -26,5 +24,5 @@ func SendEmail(w http.ResponseWriter, r *http.Request) {
 		log.Printf("smtp error: %s", err)
 		return
 	}
-	log.Print("sent, visit http://foobarbazz.mailinator.com")
+	log.Print("Email was sent")
 }
